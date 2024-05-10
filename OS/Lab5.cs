4be0768ad17cca826a90c2cc6b65baa4dc7d4eab
Ahmed Adel Elsayed Ali Basha <100494472+ahmed1672003@users.noreply.github.com>
2024-05-10 21:29:40 +0300

    commit

---
+++ OS/Lab5.cs
@@ -0,0 +1,46 @@
+﻿namespace OS;
+
+public class Lab5
+{
+    public static void CalcWatingTime(int[] processes, int n, int[] bt, int[] wt)
+    {
+        wt[0] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            wt[i] = bt[i - 1] + wt[i - 1];
+        }
+    }
+
+    public static void CalcTurnAroundTime(int[] processes, int n, int[] bt, int[] wt, int[] tat)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            tat[i] = bt[i] + wt[i];
+        }
+    }
+
+    public static void CalcAverageTime(int[] processes, int n, int[] bt)
+    {
+        int[] wt = new int[n];
+        int[] tat = new int[n];
+        int total_wt = 0,
+            total_tat = 0;
+        CalcWatingTime(processes, n, bt, wt);
+        CalcTurnAroundTime(processes, n, bt, wt, tat);
+        Console.Write("Processes Burst time Waiting" + " time Turn around time\n");
+        for (int i = 0; i < n; i++)
+        {
+            total_wt = total_wt + wt[i];
+            total_tat = total_tat + tat[i];
+            Console.Write(" {0} ", (i + 1));
+            Console.Write(" {0} ", bt[i]);
+            Console.Write(" {0}", wt[i]);
+            Console.Write(" {0}\n", tat[i]);
+        }
+        float s = (float)total_wt / (float)n;
+        int t = total_tat / n;
+        Console.Write("Average waiting time = {0}", s);
+        Console.Write("\n");
+        Console.Write("Average turn around time = {0} ", t);
+    }
+}
--- OS/Program.cs
+++ OS/Program.cs
@@ -1,20 +1,110 @@
-﻿using OS;
-
-internal class Program
+﻿internal class Program
 {
-    private static async Task Main(string[] args)
+    static void Main(string[] args)
     {
-        Fork fork = new();
-        new Philosopher(0, 1, 1, fork);
-        new Philosopher(1, 2, 2, fork);
-        new Philosopher(2, 3, 3, fork);
-        new Philosopher(3, 4, 4, fork);
-        new Philosopher(4, 5, 5, fork);
-        new Philosopher(5, 5, 5, fork);
-        new Philosopher(6, 5, 5, fork);
-        new Philosopher(7, 5, 5, fork);
-        new Philosopher(8, 5, 5, fork);
-        new Philosopher(9, 5, 5, fork);
-        new Philosopher(10, 5, 5, fork);
+        List<VirtualProcess> processes = new() { new(0, 10) };
+        // FCFSScheduler.CalcFinishTime(processes);
+        RoundRobinScheduler.ExecuteRoundRobin(processes, 2);
+        foreach (var item in processes)
+        {
+            Console.WriteLine(item.FinishTime);
+        }
     }
+}
+
+public class FCFSScheduler
+{
+    public static void CalcFinishTime(List<VirtualProcess> processes)
+    {
+        processes.ForEach(process =>
+        {
+            process.FinishTime =
+                processes.Where(x => x.ArrivalTime < process.ArrivalTime).Sum(x => x.BurstTime)
+                + process.BurstTime;
+        });
+    }
+
+    public static void CalcTurnAroundTime(List<VirtualProcess> processes)
+    {
+        processes.ForEach(process =>
+        {
+            process.ArrivalTime = process.FinishTime - process.ArrivalTime;
+        });
+    }
+
+    public static void CalcWaitingTime(List<VirtualProcess> processes)
+    {
+        processes.ForEach(process =>
+        {
+            process.WaitingTime = process.TurnAroundTime - process.BurstTime;
+        });
+    }
+}
+
+public class RoundRobinScheduler
+{
+    public static void ExecuteRoundRobin(List<VirtualProcess> processes, int timeQuantum)
+    {
+        int currentTime = 0;
+        int totalWaitingTime = 0;
+        int totalTurnaroundTime = 0;
+
+        Queue<VirtualProcess> processQueue = new Queue<VirtualProcess>(processes);
+
+        while (processQueue.Count > 0)
+        {
+            VirtualProcess currentProcess = processQueue.Dequeue();
+
+            // Execute current process for the time quantum or remaining burst time, whichever is smaller
+            int executionTime = Math.Min(timeQuantum, currentProcess.BurstTime);
+            currentProcess.BurstTime -= executionTime;
+            currentTime += executionTime;
+
+            // Update waiting time for all processes in the queue
+            foreach (var process in processQueue)
+            {
+                process.WaitingTime += executionTime;
+            }
+
+            // If current process still has burst time left, enqueue it back to the end of the queue
+            if (currentProcess.BurstTime > 0)
+            {
+                processQueue.Enqueue(currentProcess);
+            }
+            else
+            {
+                // Calculate turnaround time for the completed process
+                currentProcess.TurnAroundTime = currentTime - currentProcess.ArrivalTime;
+                totalWaitingTime += currentProcess.WaitingTime;
+                totalTurnaroundTime += currentProcess.TurnAroundTime;
+            }
+        }
+
+        // Calculate average time
+        double averageWaitingTime = (double)totalWaitingTime / processes.Count;
+        double averageTurnaroundTime = (double)totalTurnaroundTime / processes.Count;
+
+        // Assign average time to each process (for display purpose)
+        foreach (var process in processes)
+        {
+            process.AverageTime = averageTurnaroundTime;
+        }
+    }
+}
+
+public class VirtualProcess
+{
+    public VirtualProcess(int arrivalTime, int burstTime)
+    {
+        ArrivalTime = arrivalTime;
+        BurstTime = burstTime;
+    }
+
+    public int ArrivalTime { get; set; }
+    public int BurstTime { get; set; }
+    public int WaitingTime { get; set; }
+    public int TurnAroundTime { get; set; }
+    public int FinishTime { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double AverageTime { get; set; }
 }
